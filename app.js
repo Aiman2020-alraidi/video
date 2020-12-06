96b5d18408ad58d1a391073cadc1a4252321c880
Aiman Al-Raidi <aymanalredi66@gmail.com>
2020-12-06 08:37:42 +0300

    maked some change

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const videoContainer = document.querySelector(".video-container");
 const loading = document.querySelector(".loading");
 
 console.log(switchBtn);
-switchBtn.addEventListener("click", (e) => {
-  if (!e.currentTarget.classList.contains("slide")) {
-    e.currentTarget.classList.add("slide");
-    videoPause();
-  } else {
-    e.currentTarget.classList.remove("slide");
-    videoPlay();
-  }
+window.addEventListener("DOMContentLoaded", () => {
+  switchBtn.addEventListener("click", (e) => {
+    if (!e.currentTarget.classList.contains("slide")) {
+      e.currentTarget.classList.add("slide");
+      videoPause();
+    } else {
+      e.currentTarget.classList.remove("slide");
+      videoPlay();
+    }
+  });
 });
-
 const videoPlay = (e) => {
   videoContainer.play();
 };
