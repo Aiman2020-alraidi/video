e509c83aef12dc8cef0973089e03f1ee6a94614f
Aiman Al-Raidi <aymanalredi66@gmail.com>
2020-12-06 08:27:18 +0300

    added loading gif

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const switchBtn = document.querySelector(".switch-btn");
 const videoContainer = document.querySelector(".video-container");
+const loading = document.querySelector(".loading");
 
 console.log(switchBtn);
 switchBtn.addEventListener("click", (e) => {
-  if (e.currentTarget.classList.contains("slide")) {
-    e.currentTarget.classList.remove("slide");
-    videoPlay();
-  } else {
+  if (!e.currentTarget.classList.contains("slide")) {
     e.currentTarget.classList.add("slide");
     videoPause();
+  } else {
+    e.currentTarget.classList.remove("slide");
+    videoPlay();
   }
 });
 
@@ -19,3 +20,8 @@ const videoPlay = (e) => {
 const videoPause = (e) => {
   videoContainer.pause();
 };
+
+// loading when site is load
+window.addEventListener("load", (e) => {
+  loading.classList.add("hide-loading");
+});
